ff9d9e639663df2fc477b79556e6ccc666e8ea6f
Garik <khvatik.igor@gmail.com>
2024-10-28 18:24:49 +0300

    base client mvp done

--- iiko_api/core/base_client.py
+++ iiko_api/core/base_client.py
@@ -1,4 +1,13 @@
+# TODO: Флаг ис авторайз, отображает стэйт авторизации, если он тру то отправляем запрос
+# TODO: Если фолс то предварительно авторизуемся. Такжн хэндлер ответов в случае анаотарайзд меняет флаг на folse
+# TODO: При этом необходимо избежать зацыкленного повтореня попыток авторизации
+
+
 import requests
+from requests.exceptions import HTTPError, ConnectionError, Timeout, RequestException
+from iiko_api.core.config.logging_config import get_logger
+
+logger = get_logger(name=__name__)
 
 
 class BaseClient:
@@ -7,23 +16,58 @@ class BaseClient:
         self.secret = hash_password
         self.username = login
         self.session = requests.Session()
-        self.token = None
 
-    def login(self):
-        endpoint = "/resto/api/auth"
-        url = f"{self.base_url}{endpoint}"
-        params = {"login": self.username, "pass": self.secret}
-        self.token = self.session.get(url=url, params=params).text
-        print(self.token)
+    @staticmethod
+    def _handle_request_errors(func: callable):
+        """
+        Декоратор для обработки ошибок HTTP запросов
+        :param func: Функция для обработки ошибок
+        :return:
+        """
 
-    def logout(self):
-        endpoint = "/resto/api/logout"
-        params = {"token": self.token}
-        r = self.session.get(f"{self.base_url}{endpoint}", params=params)
-        print(r.text)
+        def wrapper(*args, **kwargs):
+            try:
+                response = func(*args, **kwargs)
+                response.raise_for_status()
+                return response
+            except HTTPError as http_error:
+                logger.error(f"HTTP error: {http_error} - Status code: {http_error.response.status_code}")
+                logger.debug(f"Request URL: {http_error.response.url}")
+                logger.debug(f"Request Headers: {http_error.response.request.headers}")
+                logger.debug(f"Request Body: {http_error.response.request.body}")
+                logger.debug(f"Response Headers: {http_error.response.headers}")
+                logger.debug(f"Response Body: {http_error.response.text}")
+            except ConnectionError as connection_error:
+                logger.error(f"Connection error: {connection_error}")
+            except Timeout as timeout_error:
+                logger.error(f"Timeout error: {timeout_error}")
+            except RequestException as request_error:
+                logger.error(f"Request error: {request_error}")
+            return None
 
+        return wrapper
+
+    @_handle_request_errors
     def _get(self, endpoint, params=None):
         return self.session.get(self.base_url + endpoint, params=params)
 
+    @_handle_request_errors
     def _post(self, endpoint, data=None):
         return self.session.post(self.base_url + endpoint, data=data)
+
+    def login(self):
+        endpoint = "/resto/api/auth"
+        params = {"login": self.username, "pass": self.secret}
+        response = self._get(endpoint=endpoint, params=params).text
+        if response:
+            logger.info("Авторизация прошла успешно")
+        else:
+            logger.error("Ошибка авторизации")
+
+    def logout(self):
+        endpoint = "/resto/api/logout"
+        response = self._get(endpoint=endpoint)
+        if response:
+            logger.info("Токен авторизации отменен")
+        else:
+            logger.error("Ошибка отмены авторизации")
