8093cc327fc715c08918e9008f581ccd6bfc893f
Garik <khvatik.igor@gmail.com>
2024-10-31 01:31:35 +0300

    add fill_employees_table

--- app/helpers/helpers.py
+++ app/helpers/helpers.py
@@ -1,3 +1,4 @@
+from PySide6.QtWidgets import QTableWidgetItem
 from sqlalchemy.orm import Session
 from typing import Dict, Any
 
@@ -36,6 +37,7 @@ def save_employee(session: Session, employee_data: Dict[str, Any]) -> None:
     session.commit()
 
 
+
 def assign_motivation_program(session: Session, employee_id: int, motivation_program_id: int) -> None:
     """
     Назначает мотивационную программу сотруднику.
@@ -59,3 +61,27 @@ def assign_motivation_program(session: Session, employee_id: int, motivation_pro
 
     # Сохраняем изменения
     session.commit()
+
+
+def fill_employees_table(table_widget, employees_list: list[Employee]):
+    """
+    Заполняет таблицу сотрудников
+    :param table_widget: Таблица которую необходимо заполнить.
+    :param employees_list: Список сотрудников.
+    """
+    table_widget.setRowCount(len(employees_list))
+    table_widget.setColumnCount(3)  # ID, Name, Departments
+
+    # Задаем заголовки столбцов
+    table_widget.setHorizontalHeaderLabels(['ID', 'Name', 'Departments'])
+
+    for row, employee in enumerate(employees_list):
+        table_widget.setItem(row, 0, QTableWidgetItem(str(employee.id)))
+        table_widget.setItem(row, 1, QTableWidgetItem(employee.name))
+        # Объединяем департаменты в строку, если нужно
+        departments_str = ', '.join(employee.departments)
+        table_widget.setItem(row, 2, QTableWidgetItem(departments_str))
+
+        # Выравнивание текста по центру
+        for col in range(3):
+            table_widget.item(row, col).setTextAlignment(Qt.AlignCenter)
