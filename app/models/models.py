4b812d35074a4136a7bfc8e6499bfb902f1d58eb
Garik <khvatik.igor@gmail.com>
2024-10-30 20:43:45 +0300

    some refactor and add association_table

--- app/models/models.py
+++ app/models/models.py
@@ -1,38 +1,66 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
-
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
+# Вспомогательная таблица для связи многие ко многим
+association_table = Table(
+    'employee_department_association',
+    Base.metadata,
+    Column('employee_id', Integer, ForeignKey('employees.id')),
+    Column('department_id', Integer, ForeignKey('departments.id'))
+)
+
+
 class Department(Base):
-    __tablename__ = 'department'
+    """Таблица отделов"""
+    __tablename__ = 'departments'
+
     id = Column(Integer, primary_key=True)
+    code = Column(String, unique=True, nullable=False)
     name = Column(String(250), nullable=False)
-    employees = relationship("Employee", back_populates="department")
+
+    employees = relationship(
+        "Employee",
+        secondary=association_table,
+        back_populates="departments"
+    )
 
 
 class Employee(Base):
-    __tablename__ = 'employee'
+    """Таблица сотрудников"""
+    __tablename__ = 'employees'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    # ForeignKey('department.id')
-    department_id = Column(Integer, ForeignKey('department.id'))
-    department = relationship("Department", back_populates="employees")
+    motivation_program_id = Column(Integer, ForeignKey('motivation_programs.motivation_id'))
+
+    departments = relationship(
+        "Department",
+        secondary=association_table,
+        back_populates="employees"
+    )
+    motivation_program = relationship("MotivationProgram", back_populates="employees")
 
 
 class MotivationProgram(Base):
-    __tablename__ = 'motivation_program'
+    """Таблица мотивационных программ"""
+    __tablename__ = 'motivation_programs'
+
     motivation_id = Column(Integer, primary_key=True)
     motivation_name = Column(String(250), nullable=False)
+
     employees = relationship("Employee", back_populates="motivation_program")
     thresholds = relationship("MotivateThreshold", back_populates="motivation_program")
 
+
 class MotivateThreshold(Base):
-    __tablename__ = 'motivate_threshold'
+    """Таблица уровней мотивационных программ"""
+    __tablename__ = 'motivate_thresholds'
+
     threshold_id = Column(Integer, primary_key=True)
-    # ForeignKey('motivation_program.motivation_id')
-    motivation_program_id = Column(Integer, ForeignKey('motivation_program.motivation_id'))
+    motivation_program_id = Column(Integer, ForeignKey('motivation_programs.motivation_id'))
     # Порог при достижении которого получаем вознаграждение
     revenue_threshold = Column(Integer, nullable=False)
     # Вознаграждение при достижении порога
